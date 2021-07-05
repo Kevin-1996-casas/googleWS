13a1ead070b157cc92f0bb8b2b6ac86ab30a6c51
Kevin-1996-casas <60677753+Kevin-1996-casas@users.noreply.github.com>
2021-07-05 16:11:29 -0500

    Update handler.php

--- google_pruebas/handler.php
+++ google_pruebas/handler.php
@@ -1,6 +1,8 @@
-init_set('allow_url_fopen',1);
 switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
     case '/':
+        require 'index.php';
+        break;
+    case '/index.php':
         require 'index.php';
         break;
     case '/g20_conexion_p':
