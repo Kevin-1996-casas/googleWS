0ddf0ea50c78f009eb2b9028428c418fab5164e6
Kevin-1996-casas <60677753+Kevin-1996-casas@users.noreply.github.com>
2021-07-05 20:20:19 -0500

    Update g20_conexion.php

--- google_pruebas/g20_conexion.php
+++ google_pruebas/g20_conexion.php
@@ -2,11 +2,12 @@
 
 //$conexion = new mysqli("myphpapp-318701:southamerica-east1:grupo20sistema", "root", "grupo20sistema", "grupo20sistema");
 //$conexion = new mysqli("NS01.000WEBHOST.COM","id16023351_root",'aD)tb\m)~zQ~7NJ',"id16023351_grupo20sistema");
-$dbName = 'grupo20sistema';
-$dbUser = 'root';
-$dbPass = 'grupo20sistema';
+$dbName = getenv('CLOUDSQL_DB');
+$dbUser = getenv('CLOUDSQL_USER');
+$dbPass = getenv('CLOUDSQL_PASSWORD');
+$dbDns = getenv('CLOUDSQl_DSN');
 //$conexion = new mysqli('34.95.235.163', "root", "grupo20sistema", "grupo20sistema", 3306);
-$conexion = new mysqli('127.0.0.1', $dbUser, $dbPass, $dbName, 3306);
+$conexion = new mysqli('127.0.0.1', $dbUser, $dbPass, $dbName, $dbDns);
 if ($conexion->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
 }else{
